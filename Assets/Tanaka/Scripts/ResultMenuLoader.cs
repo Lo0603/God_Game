ccdfdf73539b290ec116e25b442807d0c8f1151d
a_Tanaka <ohs30095@ohs.hal.ac.jp>
2025-06-30 11:34:18 +0900

    /

--- Assets/Tanaka/Scripts/ResultMenuLoader.cs
+++ Assets/Tanaka/Scripts/ResultMenuLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+
 
 public class ResultMenuLoader : MonoBehaviour
 {
